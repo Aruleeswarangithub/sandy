1cdf3eedc9e7d1b42d77bb26f5d94dc305c8393e
Aruleeswarangithub <arulthirumalainathan@gmail.com>
2025-05-12 19:09:54 +0530

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -20,7 +20,7 @@ async function speakText(text) {
             },
             body: JSON.stringify({
                 text: text,
-                model_id: "eleven_monolingual_v1",
+                model_id: "eleven_monolingual_v2",
                 voice_settings: {
                     stability: 0.75,
                     similarity_boost: 0.75
